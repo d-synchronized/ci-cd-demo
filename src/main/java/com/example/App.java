13aeac1a0dca85a6f63f38e72e3f925fa991a5d3
anand.dishant <anand.dishant@G1-5CD7123KX9-L.ibtechnology.com>
2021-07-09 21:03:08 +0530

    some commits

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,13 +1,23 @@
 package com.example;
 
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
+	
+	private void loadProperties() throws IOException {
+		final Properties properties = new Properties();
+    	properties.load(getClass().getClassLoader().getResourceAsStream("application.properties"));
+    	System.out.println(properties.getProperty("version"));
+	}
+	
+    public static void main( String[] args ) throws IOException
     {
-        System.out.println( "Hello World!" );
+    	new App().loadProperties();
     }
 }
